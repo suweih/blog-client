cbcc57caab7b4226d4566c3a0e60a36b75a01f5f
suweih <475464842@qq.com>
2021-05-09 19:35:02 +0800

    init

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,15 +4,20 @@
             <h1>Let's shaer</h1>
             <p>精品博客</p>
             <div class="btns">
-                <el-button>立即登录</el-button>
-                <el-button>注册账号</el-button>
+                <router-link to="/login"><el-button>立即登录</el-button></router-link>
+                <router-link to="/register"><el-button>注册账号</el-button></router-link>
             </div>
         </template>
         <template v-if="isLogin">
             <h1>Let's shaer</h1>
-            <p>精品博客</p>
             <i class="edit el-icon-edit"></i>
-            <img class="avatar" :src="user.username" :alt="user.username" :title="user.username">
+            <div class="user">
+                <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username">
+                <ul>
+                    <li><router-link to="my">我的</router-link></li>
+                    <li><a href="#" @click="onLogout">注销</a></li>
+                </ul>
+            </div>
         </template>
         
     </header>
@@ -36,9 +41,16 @@ export default {
     },
     methods:{
         ...mapActions([
-            'chechLogin'
-        ])
+            'checkLogin',
+            'logout'
+        ]),
+        onLogout(){
+        this.logout()
     }
+    },
+    created(){
+        this.checkLogin()
+    },
 }
 </script>
 
@@ -90,6 +102,34 @@ header.login{
         border:1px solid grey;
         border-radius: 50%;
         margin-left: 15px;
+    }
+
+    .user{
+        position: relative;
+        ul{
+            display: none;
+            position: absolute;
+            right: 0;
+            list-style: none;
+            border:1px solid #eaeaea;
+            margin: 0;
+            padding: 0;
+            background-color: #fff;
+            a{
+                text-decoration: none;
+                color: #333;
+                font-size: 12px;
+                display: block;
+                padding: 5px 10px;
+                
+                &:hover{
+                    background-color: #eaeaea;
+                }
+            }
+        }
+        &:hover ul{
+            display: block;
+        }
     }
 }
 </style>
